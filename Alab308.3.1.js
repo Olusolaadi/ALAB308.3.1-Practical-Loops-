1fded08e9c3ebb7537de72ad1e1040a00d24f7e8
Olusolaadi <olusolafatunde@gmail.com>
2024-08-09 20:40:20 -0500

    I still need to do more study

--- Alab308.3.1.js
+++ Alab308.3.1.js
@@ -37,6 +37,35 @@ console.log("Part 2: Prime Time")
 //As soon as you find the prime number, log that number and exit the loop.
 //Continuing with the example above, if n is equal to 4, your loop should log 5. Similarly, if n is 5, it should log 7, and if n is 9, it should log 11. 
 
+const n = 55;
+let prime = true;
+for (let q =2; q <= Math.sqrt(n); q++) {
+    if (n % q ==0 ) {
+        prime = false;
+        break;
+    }
+
+}
+
+if (prime) {
+    console.log(n);
+}
+
+//Your task is to write a script that accomplishes the following:
+//oop through the characters of a given CSV string.
+//Store each “cell” of data in a variable.
+//When you encounter a comma, move to the next cell.
+//When you encounter the “\r\n” sequence, move to the next “row.”
+//Log each row of data.
+//You do not need to format the data, the following works well.
+//console.log(cell1, cell2, cell3, cell4);
+
+
+var str = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
+for (let k = 0; k < str.length; k++) {
+   str.split(',').join('\n');
+   console.log(str[k]);
+}
 
 
 
